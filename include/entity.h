c35351a90879e2f45f2c04e335c51f0685756e42
Markus Grigull <web@grigull.me>
2018-10-09 19:43:19 +0200

    Add entity with some methods

---
+++ include/entity.h
@@ -0,0 +1,58 @@
+/*
+ * File: entity.h
+ * Author: MarkAtk
+ * Date: 09.10.2018
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Rage-MP-C-SDK
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <ragemp-cppsdk/rage.hpp>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+RAGE_API uint32_t Entity_GetId(rage::IEntity *entity);
+RAGE_API uint32_t Entity_GetType(rage::IEntity *entity);
+RAGE_API void Entity_Destroy(rage::IEntity *entity);
+RAGE_API uint32_t Entity_GetDimension(rage::IEntity *entity);
+RAGE_API void Entity_SetDimension(rage::IEntity *entity, uint32_t dimension);
+// GetPosition
+// SetPosition
+// GetRotation
+// SetRotation
+RAGE_API uint32_t Entity_GetModel(rage::IEntity *entity);
+RAGE_API void Entity_SetModel(rage::IEntity *entity, uint32_t model);
+// GetVelocity
+RAGE_API uint32_t Entity_GetAlpha(rage::IEntity *entity);
+RAGE_API void Entity_SetAlpha(rage::IEntity *entity, uint32_t alpha);
+// GetVariable
+// SetVariable
+// SetVariables
+// HasVariable
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/entity.cpp
@@ -0,0 +1,77 @@
+/*
+ * File: entity.cpp
+ * Author: MarkAtk
+ * Date: 09.10.2018
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Rage-MP-C-SDK
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "entity.h"
+
+uint32_t Entity_GetId(rage::IEntity *entity) {
+    return entity->GetId();
+}
+
+uint32_t Entity_GetType(rage::IEntity *entity) {
+    return (uint32_t)entity->GetType();
+}
+
+void Entity_Destroy(rage::IEntity *entity) {
+    entity->Destroy();
+}
+
+uint32_t Entity_GetDimension(rage::IEntity *entity) {
+    return entity->GetDimension();
+}
+
+void Entity_SetDimension(rage::IEntity *entity, uint32_t dimension) {
+    entity->SetDimension(dimension);
+}
+
+// GetPosition
+// SetPosition
+// GetRotation
+// SetRotation
+
+uint32_t Entity_GetModel(rage::IEntity *entity) {
+    return entity->GetModel();
+}
+
+void Entity_SetModel(rage::IEntity *entity, uint32_t model) {
+    entity->SetModel(model);
+}
+
+// GetVelocity
+
+uint32_t Entity_GetAlpha(rage::IEntity *entity) {
+    return entity->GetAlpha();
+}
+
+void Entity_SetAlpha(rage::IEntity *entity, uint32_t alpha) {
+    entity->SetAlpha(alpha);
+}
+
+// GetVariable
+// SetVariable
+// SetVariables
+// HasVariable
