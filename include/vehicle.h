ef2b8ccfc4b701172086fdb422be0693a19f0306
Markus Grigull <web@grigull.me>
2018-10-09 23:39:31 +0200

    Add vehicle with most methods implemented

---
+++ include/vehicle.h
@@ -0,0 +1,106 @@
+/*
+ * File: vehicle.h
+ * Author: MarkAtk
+ * Date: 09.10.2018
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Rage-MP-C-SDK
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <ragemp-cppsdk/rage.hpp>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+RAGE_API bool Vehicle_IsSirenActive(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetSirenActive(rage::IVehicle *vehicle, bool toggle);
+RAGE_API bool Vehicle_IsHornActive(rage::IVehicle *vehicle);
+RAGE_API bool Vehicle_AreHighbeamsActive(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetHighbeamsActive(rage::IVehicle *vehicle, bool toggle);
+RAGE_API bool Vehicle_AreLightsActive(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetLightsActive(rage::IVehicle *vehicle, bool toggle);
+RAGE_API bool Vehicle_IsEngineActive(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetEngineActive(rage::IVehicle *vehicle, bool toggle);
+RAGE_API bool Vehicle_AreTaxiLightsActive(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetTaxiLightsActive(rage::IVehicle *vehicle, bool toggle);
+RAGE_API bool Vehicle_IsRocketBoostActive(rage::IVehicle *vehicle);
+RAGE_API bool Vehicle_IsBrakeActive(rage::IVehicle *vehicle);
+RAGE_API float Vehicle_GetSteerAngle(rage::IVehicle *vehicle);
+RAGE_API float Vehicle_GetGasPedalState(rage::IVehicle *vehicle);
+RAGE_API float Vehicle_GetEngineHealth(rage::IVehicle *vehicle);
+RAGE_API float Vehicle_GetBodyHealth(rage::IVehicle *vehicle);
+RAGE_API rage::IPlayer *Vehicle_GetOccupant(rage::IVehicle *vehicle, uint32_t seat);
+RAGE_API void Vehicle_GetOccupants(rage::IVehicle *vehicle, rage::IPlayer **players, uint32_t *count);
+RAGE_API void Vehicle_SetOccupant(rage::IVehicle *vehicle, uint32_t seat, rage::IPlayer *player);
+RAGE_API bool Vehicle_IsLocked(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_Lock(rage::IVehicle *vehicle, bool toggle);
+RAGE_API bool Vehicle_IsDead(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_Explode(rage::IVehicle *vehicle);
+//RAGE_API void Vehicle_Spawn(rage::IVehicle *vehicle, pos, float heading);
+RAGE_API uint32_t Vehicle_GetMod(rage::IVehicle *vehicle, uint32_t id);
+RAGE_API void Vehicle_SetMod(rage::IVehicle *vehicle, uint32_t id, uint32_t mod);
+RAGE_API bool Vehicle_AreNeonsEnabled(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_EnableNeons(rage::IVehicle *vehicle, bool toggle);
+RAGE_API void Vehicle_SetNeonsColor(rage::IVehicle *vehicle, uint32_t red, uint32_t green, uint32_t blue);
+// GetNeonsColor
+RAGE_API void Vehicle_Repair(rage::IVehicle *vehicle);
+// GetColorRGB
+RAGE_API uint32_t Vehicle_GetColor(rage::IVehicle *vehicle, uint32_t id);
+RAGE_API uint32_t Vehicle_GetPaint(rage::IVehicle *vehicle, uint32_t id);
+// SetColorRGB
+RAGE_API void Vehicle_SetColor(rage::IVehicle *vehicle, uint32_t primary, uint32_t secondary);
+// SetPaint
+RAGE_API uint32_t Vehicle_GetMaterialType(rage::IVehicle *vehicle);
+RAGE_API const char *Vehicle_GetNumberPlate(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetNumberPlate(rage::IVehicle *vehicle, const char *numberPlate);
+RAGE_API bool Vehicle_IsStreamed(rage::IVehicle *vehicle, rage::IPlayer *player);
+RAGE_API void Vehicle_GetStreamed(rage::IVehicle *vehicle, rage::IPlayer ***players, uint32_t *count);
+RAGE_API uint32_t Vehicle_GetLivery(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetLivery(rage::IVehicle *vehicle, uint32_t livery);
+RAGE_API uint32_t Vehicle_GetWheelColor(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetWheelColor(rage::IVehicle *vehicle, uint32_t color);
+RAGE_API uint32_t Vehicle_GetWheelType(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetWheelType(rage::IVehicle *vehicle, uint32_t type);
+RAGE_API uint32_t Vehicle_GetNumberPlateType(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetNumberPlateType(rage::IVehicle *vehicle, uint32_t type);
+RAGE_API uint32_t Vehicle_GetPearlescentColor(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetPearlescentColor(rage::IVehicle *vehicle, uint32_t color);
+RAGE_API uint32_t Vehicle_GetWindowTint(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetWindowTint(rage::IVehicle *vehicle, uint32_t tint);
+RAGE_API uint32_t Vehicle_GetDashboardColor(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetDashboardColor(rage::IVehicle *vehicle, uint32_t color);
+RAGE_API uint32_t Vehicle_GetTrimColor(rage::IVehicle *vehicle);
+RAGE_API void Vehicle_SetTrimColor(rage::IVehicle *vehicle, uint32_t color);
+RAGE_API bool Vehicle_GetExtra(rage::IVehicle *vehicle, uint32_t id);
+RAGE_API void Vehicle_SetExtra(rage::IVehicle *vehicle, uint32_t id, bool state);
+RAGE_API float Vehicle_GetMovableState(rage::IVehicle *vehicle);
+// GetQuaternion
+RAGE_API float Vehicle_GetHeading(rage::IVehicle *vehicle);
+RAGE_API rage::IVehicle *Vehicle_GetTrailer(rage::IVehicle *vehicle);
+RAGE_API rage::IVehicle *Vehicle_GetTraileredBy(rage::IVehicle *vehicle);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/vehicle.cpp
@@ -0,0 +1,295 @@
+/*
+ * File: vehicle.cpp
+ * Author: MarkAtk
+ * Date: 09.10.2018
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 Rage-MP-C-SDK
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "vehicle.h"
+
+bool Vehicle_IsSirenActive(rage::IVehicle *vehicle) {
+    return vehicle->IsSirenActive();
+}
+
+void Vehicle_SetSirenActive(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->SetSirenActive(toggle);
+}
+
+bool Vehicle_IsHornActive(rage::IVehicle *vehicle) {
+    return vehicle->IsHornActive();
+}
+
+bool Vehicle_AreHighbeamsActive(rage::IVehicle *vehicle) {
+    return vehicle->AreHighbeamsActive();
+}
+
+void Vehicle_SetHighbeamsActive(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->SetHighbeamsActive(toggle);
+}
+
+bool Vehicle_AreLightsActive(rage::IVehicle *vehicle) {
+    return vehicle->AreLightsActive();
+}
+
+void Vehicle_SetLightsActive(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->SetLightsActive(toggle);
+}
+
+bool Vehicle_IsEngineActive(rage::IVehicle *vehicle) {
+    return vehicle->IsEngineActive();
+}
+
+void Vehicle_SetEngineActive(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->SetEngineActive(toggle);
+}
+
+bool Vehicle_AreTaxiLightsActive(rage::IVehicle *vehicle) {
+    return vehicle->AreTaxiLightsActive();
+}
+
+void Vehicle_SetTaxiLightsActive(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->SetTaxiLightsActive(toggle);
+}
+
+bool Vehicle_IsRocketBoostActive(rage::IVehicle *vehicle) {
+    return vehicle->IsRocketBoostActive();
+}
+
+bool Vehicle_IsBrakeActive(rage::IVehicle *vehicle) {
+    return vehicle->IsBrakeActive();
+}
+
+float Vehicle_GetSteerAngle(rage::IVehicle *vehicle) {
+    return vehicle->GetSteerAngle();
+}
+
+float Vehicle_GetGasPedalState(rage::IVehicle *vehicle) {
+    return vehicle->GetGasPedalState();
+}
+
+float Vehicle_GetEngineHealth(rage::IVehicle *vehicle) {
+    return vehicle->GetEngineHealth();
+}
+
+float Vehicle_GetBodyHealth(rage::IVehicle *vehicle) {
+    return vehicle->GetBodyHealth();
+}
+
+rage::IPlayer *Vehicle_GetOccupant(rage::IVehicle *vehicle, uint32_t seat) {
+    return vehicle->GetOccupant(seat);
+}
+
+void Vehicle_GetOccupants(rage::IVehicle *vehicle, rage::IPlayer ***players, uint32_t *count) {
+    auto occupantVector = vehicle->GetOccupants();
+
+    *players = (rage::IPlayer **)malloc(occupantVector.size() * sizeof(rage::IPlayer *));
+    *count = (uint32_t)occupantVector.size();
+
+    for (int i = 0; i < occupantVector.size(); i++) {
+        *players[i] = occupantVector[i];
+    }
+}
+
+void Vehicle_SetOccupant(rage::IVehicle *vehicle, uint32_t seat, rage::IPlayer *player) {
+    vehicle->SetOccupant(seat, player);
+}
+
+bool Vehicle_IsLocked(rage::IVehicle *vehicle) {
+    return vehicle->IsLocked();
+}
+
+void Vehicle_Lock(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->Lock(toggle);
+}
+
+bool Vehicle_IsDead(rage::IVehicle *vehicle) {
+    return vehicle->IsDead();
+}
+
+void Vehicle_Explode(rage::IVehicle *vehicle) {
+    vehicle->Explode();
+}
+
+//void Vehicle_Spawn(rage::IVehicle *vehicle, pos, float heading)
+
+uint32_t Vehicle_GetMod(rage::IVehicle *vehicle, uint32_t id) {
+    return vehicle->GetMod(id);
+}
+
+void Vehicle_SetMod(rage::IVehicle *vehicle, uint32_t id, uint32_t mod) {
+    vehicle->SetMod(id, mod);
+}
+
+bool Vehicle_AreNeonsEnabled(rage::IVehicle *vehicle) {
+    return vehicle->AreNeonsEnabled();
+}
+
+void Vehicle_EnableNeons(rage::IVehicle *vehicle, bool toggle) {
+    vehicle->EnableNeons(toggle);
+}
+
+void Vehicle_SetNeonsColor(rage::IVehicle *vehicle, uint32_t red, uint32_t green, uint32_t blue) {
+    vehicle->SetNeonsColour(red, green, blue);
+}
+
+// GetNeonsColor
+
+void Vehicle_Repair(rage::IVehicle *vehicle) {
+    vehicle->Repair();
+}
+
+// GetColorRGB
+
+uint32_t Vehicle_GetColor(rage::IVehicle *vehicle, uint32_t id) {
+    return vehicle->GetColour(id);
+}
+
+uint32_t Vehicle_GetPaint(rage::IVehicle *vehicle, uint32_t id) {
+    return vehicle->GetPaint(id);
+}
+
+// SetColorRGB
+
+void Vehicle_SetColor(rage::IVehicle *vehicle, uint32_t primary, uint32_t secondary) {
+    vehicle->SetColour(primary, secondary);
+}
+
+// SetPaint
+
+uint32_t Vehicle_GetMaterialType(rage::IVehicle *vehicle) {
+    return (uint32_t)vehicle->GetMaterialType();
+}
+
+const char *Vehicle_GetNumberPlate(rage::IVehicle *vehicle) {
+    return vehicle->GetNumberPlate().c_str();
+}
+
+void Vehicle_SetNumberPlate(rage::IVehicle *vehicle, const char *numberPlate) {
+    vehicle->SetNumberPlate(numberPlate);
+}
+
+bool Vehicle_IsStreamed(rage::IVehicle *vehicle, rage::IPlayer *player) {
+    return vehicle->IsStreamed(player);
+}
+
+void Vehicle_GetStreamed(rage::IVehicle *vehicle, rage::IPlayer ***players, uint32_t *count) {
+    auto streamedVector = vehicle->GetStreamed();
+
+    *players = (rage::IPlayer **)malloc(streamedVector.size() * sizeof(rage::IPlayer *));
+    *count = streamedVector.size();
+
+    for (int i = 0; i < streamedVector.size(); i++) {
+        *players[i] = streamedVector[i];
+    }
+}
+
+uint32_t Vehicle_GetLivery(rage::IVehicle *vehicle) {
+    return vehicle->GetLivery();
+}
+
+void Vehicle_SetLivery(rage::IVehicle *vehicle, uint32_t livery) {
+    vehicle->SetLivery(livery);
+}
+
+uint32_t Vehicle_GetWheelColor(rage::IVehicle *vehicle) {
+    return vehicle->GetWheelColor();
+}
+
+void Vehicle_SetWheelColor(rage::IVehicle *vehicle, uint32_t color) {
+    vehicle->SetWheelColor(color);
+}
+
+uint32_t Vehicle_GetWheelType(rage::IVehicle *vehicle) {
+    return vehicle->GetWheelType();
+}
+
+void Vehicle_SetWheelType(rage::IVehicle *vehicle, uint32_t type) {
+    vehicle->SetWheelType(type);
+}
+
+uint32_t Vehicle_GetNumberPlateType(rage::IVehicle *vehicle) {
+    return vehicle->GetNumberPlateType();
+}
+
+void Vehicle_SetNumberPlateType(rage::IVehicle *vehicle, uint32_t type) {
+    vehicle->SetNumberPlateType(type);
+}
+
+uint32_t Vehicle_GetPearlescentColor(rage::IVehicle *vehicle) {
+    return vehicle->GetPearlescentColor();
+}
+
+void Vehicle_SetPearlescentColor(rage::IVehicle *vehicle, uint32_t color) {
+    vehicle->SetPearlescentColor(color);
+}
+
+uint32_t Vehicle_GetWindowTint(rage::IVehicle *vehicle) {
+    return vehicle->GetWindowTint();
+}
+
+void Vehicle_SetWindowTint(rage::IVehicle *vehicle, uint32_t tint) {
+    vehicle->SetWindowTint(tint);
+}
+
+uint32_t Vehicle_GetDashboardColor(rage::IVehicle *vehicle) {
+    return vehicle->GetDashboardColor();
+}
+
+void Vehicle_SetDashboardColor(rage::IVehicle *vehicle, uint32_t color) {
+    vehicle->SetDashboardColor(color);
+}
+
+uint32_t Vehicle_GetTrimColor(rage::IVehicle *vehicle) {
+    return vehicle->GetTrimColor();
+}
+
+void Vehicle_SetTrimColor(rage::IVehicle *vehicle, uint32_t color) {
+    vehicle->SetTrimColor(color);
+}
+
+bool Vehicle_GetExtra(rage::IVehicle *vehicle, uint32_t id) {
+    return vehicle->GetExtra(id);
+}
+
+void Vehicle_SetExtra(rage::IVehicle *vehicle, uint32_t id, bool state) {
+    vehicle->SetExtra(id, state);
+}
+
+float Vehicle_GetMovableState(rage::IVehicle *vehicle) {
+    return vehicle->GetMovableState();
+}
+
+// GetQuaternion
+
+float Vehicle_GetHeading(rage::IVehicle *vehicle) {
+    return vehicle->GetHeading();
+}
+
+rage::IVehicle *Vehicle_GetTrailer(rage::IVehicle *vehicle) {
+    return vehicle->GetTrailer();
+}
+
+rage::IVehicle *Vehicle_GetTraileredBy(rage::IVehicle *vehicle) {
+    return vehicle->GetTraileredBy();
+}
