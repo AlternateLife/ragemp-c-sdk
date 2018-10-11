cd8876ae28988808bf90f8de7277e3f570bbb745
Markus Grigull <web@grigull.me>
2018-10-11 23:49:17 +0200

    Add clang compiler check to rage includes

--- include/wrapper/checkpointPool.h
+++ include/wrapper/checkpointPool.h
@@ -28,7 +28,14 @@
 
 #pragma once
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wempty-body"
+#endif
 #include <ragemp-cppsdk/rage.hpp>
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 #ifdef __cplusplus
 extern "C" {
